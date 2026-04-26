2f432e5e30ccc113edd20fe250b590e0ef83478a
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-04-26 11:33:37 +0530

    Update Interview.java

--- Day3/Interview.java
+++ Day3/Interview.java
@@ -1,1 +1,7 @@
-
+What is class and object?
+Difference between object and reference?
+What is constructor?
+Can constructor be private?
+What is this keyword?
+Can we have multiple constructors?
+What happens if we don’t define constructor?
