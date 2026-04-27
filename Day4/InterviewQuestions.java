cf97a961056b3aa397088a4f76824ed7a1ea6647
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-04-27 18:01:17 +0530

    Create InterviewQuestions.java

---
+++ Day4/InterviewQuestions.java
@@ -0,0 +1,8 @@
+What is inheritance?
+Why use inheritance?
+What is method overriding?
+Difference between overloading vs overriding?
+What is runtime polymorphism?
+What is upcasting?
+Can we override static methods?
+What is @Override annotation?
